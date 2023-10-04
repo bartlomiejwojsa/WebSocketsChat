81a5cb9dc73fef94680a95c893afa7c2b3b004e1
Bartlomiej Wojsa <bartlomiej.wojsa@antsolutions.eu>
2023-10-05 01:09:56 +0200

    Initial Commit

---
+++ WebSocketsChat/WebSocketsChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebSocketsChatApp.swift
+//  WebSocketsChat
+//
+//  Created by Bartłomiej Wojsa on 05/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct WebSocketsChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
